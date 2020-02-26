cbdafc7d3c87e305060d3aeb1cf8c865d87ac15c
= <=>
2020-02-26 16:41:40 -0500

    Update ALU.

--- ALU.v
+++ ALU.v
@@ -2,30 +2,26 @@
 
 module ALU
 (
-  input [63:0] A,
-  input [63:0] B,
-  input [3:0] C,
-  output reg [63:0] R,
-  output reg ZERO
+  input [63:0] A,       // Input A
+  input [63:0] B,       // Input B
+  input [3:0] C,        // ALU Control input
+  output reg [63:0] R,  // ALU Result
+  output wire ZERO       // Outputs true if RES is zero
 );
+
+
 
   always @(*) begin
     case (C)
-      4'b0000 : R = A & B;
-      4'b0001 : R = A | B;
-      4'b0010 : R = A + B;
-      4'b0110 : R = A - B;
-      4'b0111 : R = B;
-      4'b1100 : R = 64'hxxxxxxxx;
+      4'b0000 : R <= A & B;
+      4'b0001 : R <= A | B;
+      4'b0010 : R <= A + B;
+      4'b0110 : R <= A - B;
+      4'b0111 : R <= B;
+      4'b1100 : R <= 64'hxxxxxxxx;
     endcase
-
-    if (R == 0) begin
-      ZERO = 1'b1;
-    end else if (R != 0) begin
-      ZERO = 1'b0;
-    end else begin
-      ZERO = 1'bx;
-    end
   end
-  
+
+  assign ZERO = (B == 'h00) ? 'b1 : 'b0;
+
 endmodule
